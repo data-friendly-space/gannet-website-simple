f12205a54762d96992e5dbcb59adaf2a102c0cbd
nayid-dfs <nayid@datafriendlyspace.org>
2025-03-25 19:00:32 -0500

    Added OnPagesInit to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
+const { withOnPagesInit } = require('@cloudflare/next-on-pages/init');
+
 const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'randomuser.me'],
   },
 };
 
-module.exports = nextConfig; 
+module.exports = withOnPagesInit(nextConfig); 
